4aa7a033a113971ecbc3775c71febca2ed12b3a7
MaStEr <masternonnolnw@gmail.com>
2021-10-08 22:43:24 +0700

    prototype_1.4

--- frontend/utils/mockApi.js
+++ frontend/utils/mockApi.js
@@ -1,4 +1,8 @@
+import axios from "axios";
+
 export const userLogin = async ({ email, password }) => {
+  // const baseURL = "http://localhost:3500";
+  // const token = await axios.post(`${baseURL}/login`, { email, password });
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (email === "master" && password === "1234") {
