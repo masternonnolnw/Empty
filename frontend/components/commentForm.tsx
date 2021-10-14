fd9eb1b077288c10e689026dc0a1b851c59126d3
MaStEr <masternonnolnw@gmail.com>
2021-10-14 22:08:36 +0700

    .

--- frontend/components/commentForm.tsx
+++ frontend/components/commentForm.tsx
@@ -1,9 +1,11 @@
 import { IconButton } from "@chakra-ui/button";
 import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 import { Box, Flex, Link, Text } from "@chakra-ui/layout";
+import moment from "moment";
 
-export default function commentForm(comment) {
-  // console.log(comment);
+export default function commentForm({ comment, postId, upLike, downLike }) {
+  console.log(comment);
+  // console.log(postId);
   return (
     <>
       <Flex
@@ -24,10 +26,10 @@ export default function commentForm(comment) {
         >
           <IconButton
             variant="ghost"
-            colorScheme={comment.comment.status == 1 ? "green" : "white"}
+            colorScheme={comment.status == 1 ? "green" : "white"}
             aria-label="Search database"
-            // onClick={() => upLike(pos)}
-            isDisabled={comment.comment.status == 99}
+            onClick={() => upLike(comment.status, postId, comment.id)}
+            isDisabled={comment.status == 99}
             icon={<ArrowUpIcon />}
             isRound
             fontSize="3xl"
@@ -39,13 +41,13 @@ export default function commentForm(comment) {
             }}
           />
 
-          <Text fontSize="2xl">{comment.comment.totallike}</Text>
+          <Text fontSize="2xl">{comment.totallike}</Text>
           <IconButton
             variant="ghost"
-            colorScheme={comment.comment.status == -1 ? "red" : "white"}
+            colorScheme={comment.status == -1 ? "red" : "white"}
             aria-label="Search database"
-            // onClick={() => downLike(pos)}
-            isDisabled={comment.comment.status == 99}
+            onClick={() => downLike(comment.status, postId, comment.id)}
+            isDisabled={comment.status == 99}
             icon={<ArrowDownIcon />}
             isRound
             fontSize="3xl"
@@ -57,7 +59,22 @@ export default function commentForm(comment) {
             }}
           />
         </Flex>
-        <Text>{comment.comment.body}</Text>
+        <Flex flexDir="column" h="100%">
+          <Flex alignSelf="flex-start">
+            <Text marginRight="5" alignSelf="center">
+              {moment(moment()).diff(comment.date, "minutes") == 0
+                ? "Just now"
+                : moment(moment()).diff(comment.date, "minutes") <= 10
+                ? "posted " +
+                  moment(moment()).diff(comment.date, "minutes") +
+                  " minutes ago"
+                : moment(comment.date).format("DD/MM/YYYY, h:mm:ss a")}
+            </Text>
+          </Flex>
+          <Flex alignSelf="center">
+            <Text>{comment.body}</Text>
+          </Flex>
+        </Flex>
       </Flex>
     </>
   );
